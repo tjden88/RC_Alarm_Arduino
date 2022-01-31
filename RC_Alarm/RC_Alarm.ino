80e259b20cfbf08c7bb0db3e4dd2a7a11c62ac18
Денис <dultsevdv@gmail.com>
2022-01-31 07:48:48 +0500

    Заготовка функций

--- RC_Alarm/RC_Alarm.ino
+++ RC_Alarm/RC_Alarm.ino
@@ -1,6 +1,10 @@
 byte PWM_PIN = 3; // Пин сигнала приёмника
 
-int pwm_value;
+byte LEFT_LED = 0; // Пин левого светодиода
+byte RIGHT_LED = 0; // Пин правого светодиода
+byte BUZZER_PIN = 0; // Пин пищалки
+
+
 
 void setup() {
 	pinMode(PWM_PIN, INPUT);
@@ -8,6 +12,47 @@ void setup() {
 }
 
 void loop() {
-	pwm_value = pulseIn(PWM_PIN, HIGH);
+
+	int pwm_value = pulseIn(PWM_PIN, HIGH);
 	Serial.println(pwm_value);
+
+	if (pwm_value < 100) {
+		leds_blink();
+		buzzer_alarm();
+	}
+	else if (pwm_value < 1250) {
+		leds_off();
+	}
+	else if (pwm_value < 1500)
+	{
+		leds_on();
+	}
+	else if (pwm_value < 1750)
+	{
+		leds_blink();
+	}
+	else if (pwm_value >= 1750)
+	{
+		leds_blink();
+		buzzer_sound();
+	}
+}
+
+void leds_blink() {
+	Serial.println("leds_blink");
+}
+
+void leds_on() {
+	Serial.println("leds_on");
+}
+
+void leds_off() {
+	Serial.println("leds_off");
+}
+
+void buzzer_sound() {
+	Serial.println("buzzer_sound");
+}
+void buzzer_alarm() {
+	Serial.println("buzzer_alarm");
 }
