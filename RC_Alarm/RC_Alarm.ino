28754f5c563577948a2941031ec9122e5e26768d
Денис <dultsevdv@gmail.com>
2022-01-31 08:38:27 +0500

    Мигание по таймерам

--- RC_Alarm/RC_Alarm.ino
+++ RC_Alarm/RC_Alarm.ino
@@ -1,10 +1,16 @@
+#include "GyverTimer.h"
+
 byte PWM_PIN = 3; // Пин сигнала приёмника
 
-byte LEFT_LED = 0; // Пин левого светодиода
-byte RIGHT_LED = 0; // Пин правого светодиода
+byte LEFT_LED = 5; // Пин левого светодиода
+byte RIGHT_LED = 6; // Пин правого светодиода
 byte BUZZER_PIN = 0; // Пин пищалки
 
 
+GTimer Timer_Long_Lat(MS, 1400); // Таймер переключения светодиодов
+GTimer Timer_Small_Lat(MS); // Таймер малой задержки светодиодов
+int small_timeout = 90; // Таймаут мигания светодиодов
+byte blinks_count = 3; // Количество миганий каждого светодиода
 
 void setup() {
 	pinMode(PWM_PIN, INPUT);
@@ -14,13 +20,9 @@ void setup() {
 void loop() {
 
 	int pwm_value = pulseIn(PWM_PIN, HIGH);
-	Serial.println(pwm_value);
+	//Serial.println(pwm_value);
 
-	if (pwm_value < 100) {
-		leds_blink();
-		buzzer_alarm();
-	}
-	else if (pwm_value < 1250) {
+	if (pwm_value < 1250) {
 		leds_off();
 	}
 	else if (pwm_value < 1500)
@@ -38,21 +40,51 @@ void loop() {
 	}
 }
 
+
+bool nowBlinkRight = false;
+bool led_now_on = false;
+byte led_blink_count = 0;
+
 void leds_blink() {
-	Serial.println("leds_blink");
+	if (Timer_Long_Lat.isReady()) {
+		nowBlinkRight = !nowBlinkRight;
+		digitalWrite(RIGHT_LED, nowBlinkRight);
+		digitalWrite(LEFT_LED, !nowBlinkRight);
+		Timer_Small_Lat.setTimeout(small_timeout);
+		led_now_on = true;
+		led_blink_count = 1;
+	}
+	led_small_blink(nowBlinkRight ? RIGHT_LED : LEFT_LED);
+}
+
+
+void led_small_blink(byte pin) {
+	if (Timer_Small_Lat.isReady()) {
+		if (led_blink_count >= blinks_count) {
+			digitalWrite(pin, LOW);
+			return;
+		}
+
+		led_now_on = !led_now_on;
+		digitalWrite(pin, led_now_on);
+
+		if (led_now_on) 
+			led_blink_count++;
+
+		Timer_Small_Lat.setTimeout(small_timeout);
+	}
 }
 
 void leds_on() {
-	Serial.println("leds_on");
+	digitalWrite(RIGHT_LED, HIGH);
+	digitalWrite(LEFT_LED, HIGH);
 }
 
 void leds_off() {
-	Serial.println("leds_off");
+	digitalWrite(RIGHT_LED, LOW);
+	digitalWrite(LEFT_LED, LOW);
 }
 
 void buzzer_sound() {
 	Serial.println("buzzer_sound");
-}
-void buzzer_alarm() {
-	Serial.println("buzzer_alarm");
 }
